e565439ef3a2243d6f875d94826003aff4731186
bhaines <bhaines@yext.com>
2021-12-24 10:23:25 -0500

    home page

---
+++ src/Orb.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Orb.scss";
+
+export default function Orb() {
+	return (
+		// <Link className="Orb" to="/board">
+		<div className="Orb">
+			<div className="wrapper">
+				<div className="loading"></div>
+			</div>
+		</div>
+		// </Link>
+	)
+}
