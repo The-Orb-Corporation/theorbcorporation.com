07bbf67b1843ea8dd5ffabebb64d6c0e189905c3
Benjamin Haines <bhaines@yext.com>
2025-07-26 19:48:49 -0400

    replace createReactSite with eleventy

---
+++ _data/metadata.js
@@ -0,0 +1,6 @@
+export default {
+	title: "The Orb Corporation",
+	url: "https://theorbcorporation.com/",
+	language: "en",
+	description: "Productions of the corporation.",
+}
--- src/Header.tsx
+++
@@ -1,9 +0,0 @@
-import "./Header.scss";
-
-export default function Header() {
-	return (
-		<header className="Header">
-			{/* The Orb Corporation */}
-		</header>
-	)
-}
